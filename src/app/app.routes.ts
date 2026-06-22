44a7c22df07af98657dde625a963093fe3aeae8b
reyesrar <reyesrainny11@gmail.com>
2026-06-22 16:59:46 -0400

    feat(pages): generate all feature pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -48,4 +48,36 @@ export const routes: Routes = [
     loadComponent: () =>
       import('./features/settings/settings.page').then((m) => m.SettingsPage),
   },
+  {
+    path: 'splash',
+    loadComponent: () => import('./features/auth/splash/splash.page').then( m => m.SplashPage)
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./features/auth/login/login.page').then( m => m.LoginPage)
+  },
+  {
+    path: 'register',
+    loadComponent: () => import('./features/auth/register/register.page').then( m => m.RegisterPage)
+  },
+  {
+    path: 'home',
+    loadComponent: () => import('./features/home/home/home.page').then( m => m.HomePage)
+  },
+  {
+    path: 'explore',
+    loadComponent: () => import('./features/explore/explore/explore.page').then( m => m.ExplorePage)
+  },
+  {
+    path: 'movie-detail',
+    loadComponent: () => import('./features/movie-detail/movie-detail/movie-detail.page').then( m => m.MovieDetailPage)
+  },
+  {
+    path: 'profile',
+    loadComponent: () => import('./features/profile/profile/profile.page').then( m => m.ProfilePage)
+  },
+  {
+    path: 'settings',
+    loadComponent: () => import('./features/settings/settings/settings.page').then( m => m.SettingsPage)
+  },
 ];
