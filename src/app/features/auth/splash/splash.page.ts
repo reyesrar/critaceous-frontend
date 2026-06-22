b9d3b761485a6e5d5731bfbe8cdd9203300e7fb1
reyesrar <reyesrainny11@gmail.com>
2026-06-22 17:03:15 -0400

    feat(splash): add splash screen with and redirect

--- src/app/features/auth/splash/splash.page.ts
+++ src/app/features/auth/splash/splash.page.ts
@@ -1,20 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
+import { IonContent } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-splash',
   templateUrl: './splash.page.html',
   styleUrls: ['./splash.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent],
 })
 export class SplashPage implements OnInit {
-
-  constructor() { }
+  constructor(private router: Router) {}
 
   ngOnInit() {
+    // Redirect to login after splash animation
+    setTimeout(() => {
+      this.router.navigate(['/login'], { replaceUrl: true });
+    }, 2500);
   }
-
 }
