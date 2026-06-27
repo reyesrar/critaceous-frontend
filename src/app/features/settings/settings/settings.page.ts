fbb362d611432923847817505ec72967e2dcf346
reyesrar <reyesrainny11@gmail.com>
2026-06-27 10:23:59 -0400

    feat(settings): add settings page with theme, audio and preferences

--- src/app/features/settings/settings/settings.page.ts
+++ src/app/features/settings/settings/settings.page.ts
@@ -1,20 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {
+  IonContent, IonHeader, IonToolbar, IonTitle,
+  IonCard, IonCardContent, IonItem, IonLabel,
+  IonToggle, IonSelect, IonSelectOption
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.page.html',
   styleUrls: ['./settings.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    CommonModule, FormsModule,
+    IonContent, IonHeader, IonToolbar, IonTitle,
+    IonCard, IonCardContent, IonItem, IonLabel,
+    IonToggle, IonSelect, IonSelectOption
+  ],
 })
-export class SettingsPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class SettingsPage {
+  // TODO: persist preferences with storage service
+  darkMode = true;
+  soundEnabled = true;
+  language = 'en';
+  defaultSort = 'popularity';
 }
