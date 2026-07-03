15743751de24b46709b7fc57fbe62dd49bf2c4d0
reyesrar <reyesrainny11@gmail.com>
2026-07-03 15:30:04 -0400

    feat(auth): connect login and register pages to AuthService

--- src/app/features/auth/login/login.page.ts
+++ src/app/features/auth/login/login.page.ts
@@ -1,26 +1,37 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import {
-  IonContent, IonInput, IonButton, IonLabel, IonItem
+  IonContent, IonInput, IonButton, IonLabel, IonItem, IonSpinner
 } from '@ionic/angular/standalone';
+import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonContent, IonInput, IonButton, IonLabel, IonItem, FormsModule],
+  imports: [CommonModule, FormsModule, IonContent, IonInput, IonButton, IonLabel, IonItem, IonSpinner],
 })
 export class LoginPage {
   email = '';
   password = '';
+  loading = false;
+  error = '';
 
-  constructor(private router: Router) {}
+  constructor(private auth: AuthService, private router: Router) {}
 
-  onLogin() {
-    // TODO: connect to auth service
-    this.router.navigate(['/app'], { replaceUrl: true });
+  async onLogin() {
+    this.error = '';
+    this.loading = true;
+    try {
+      await this.auth.login(this.email, this.password);
+    } catch {
+      this.error = 'Invalid email or password';
+    } finally {
+      this.loading = false;
+    }
   }
 
   goToRegister() {
--- src/app/features/auth/register/register.page.ts
+++ src/app/features/auth/register/register.page.ts
@@ -1,28 +1,43 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import {
-  IonContent, IonInput, IonButton, IonLabel, IonItem
+  IonContent, IonInput, IonButton, IonLabel, IonItem, IonSpinner
 } from '@ionic/angular/standalone';
+import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.scss'],
   standalone: true,
-  imports: [IonContent, IonInput, IonButton, IonLabel, IonItem, FormsModule],
+  imports: [CommonModule, FormsModule, IonContent, IonInput, IonButton, IonLabel, IonItem, IonSpinner],
 })
 export class RegisterPage {
   name = '';
   email = '';
   password = '';
   confirmPassword = '';
+  loading = false;
+  error = '';
 
-  constructor(private router: Router) {}
+  constructor(private auth: AuthService, private router: Router) {}
 
-  onRegister() {
-    // TODO: connect to auth service
-    this.router.navigate(['/app'], { replaceUrl: true });
+  async onRegister() {
+    this.error = '';
+    if (this.password !== this.confirmPassword) {
+      this.error = 'Passwords do not match';
+      return;
+    }
+    this.loading = true;
+    try {
+      await this.auth.register(this.name, this.email, this.password);
+    } catch {
+      this.error = 'Registration failed. Try again.';
+    } finally {
+      this.loading = false;
+    }
   }
 
   goToLogin() {
